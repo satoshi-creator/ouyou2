99e6b3b09f9391bce1f428efbbe3d14c6efb834e
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-19 07:46:10 +0000

    Finish

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,20 @@
 class Book < ApplicationRecord
   belongs_to :user
-  has_one_attached :profile_image  
-  
+  has_one_attached :profile_image
+
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+  validates :book_comments,presence:true
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end

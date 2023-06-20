915d3e371f2c296b48c20355978307af417a3086
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-06-20 11:41:48 +0000

    search

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  get "search" => "searches#search_result"
+  
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]

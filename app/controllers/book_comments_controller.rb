8b168655cbf59a50a72751225a2a6b010d81fe15
satoshi-creator <112931371+satoshi-creator@users.noreply.github.com>
2023-07-03 08:20:37 +0000

    comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      redirect_to book_path(book)
-    else
-      redirect_to request.referer
-    end
+    comment.book_id = @book.id
+    comment.save
+    
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comment = BookComment.find(params[:id]).destroy
+    @book_comment.destroy
+    @book = Book.find(params[:book_id])
   end
 
   private
